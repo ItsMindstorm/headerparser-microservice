81ca665ab034266c5635595f6973285ba93ab7f4
yvess <yvess.straten@gmail.com>
2023-09-09 14:42:44 +0800

    Added ability to see ip request header trough proxy, and edited the README

--- index.js
+++ index.js
@@ -13,28 +13,29 @@ var useragent = require("express-useragent");
 // so that your API is remotely testable by FCC
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.enable("trust proxy")
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/views/index.html");
 });
 
 // Register the useragent middleware
 app.use("/api/whoami", useragent.express());
 
 // Endpoint to get info
-app.get("/api/whoami", function (req, res) {
-  res.json({
-    ipaddress: req.ip,
-    language: req.headers["accept-language"],
-    software: req.useragent.source,
-  });
+app.get("/api/whoami", function(req, res) {
+	res.json({
+		ipaddress: req.ip,
+		language: req.headers["accept-language"],
+		software: req.useragent.source,
+	});
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+var listener = app.listen(process.env.PORT || 3000, function() {
+	console.log("Your app is listening on port " + listener.address().port);
 });
